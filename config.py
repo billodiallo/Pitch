7236490631a0113ff48f08b43376cdf4dc603c1c
Mamadou Billo Diallo <billodiallonet@gmail.com>
2020-05-04 21:22:38 +0300

    ADD database name

--- config.py
+++ config.py
@@ -2,6 +2,10 @@ import os
 
 class Config:
 
+    SECRET_KEY=os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://billo:123456@localhost/pitch'
+
+
     UPLOADED_PHOTOS_DEST ='app/static/photos'
 
     #  email configurations
