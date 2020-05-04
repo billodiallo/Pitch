e570e840ad38a35f1943ad0cff06af5da2b1a8bc
Mamadou Billo Diallo <billodiallonet@gmail.com>
2020-05-04 08:34:20 +0300

    configure upload photo

--- app/__init__.py
+++ app/__init__.py
@@ -17,3 +17,15 @@ bootstrap = Bootstrap()
 db = SQLAlchemy()
 photos = UploadSet('photos',IMAGES)
 mail = Mail()
+
+def create_app(config_name):
+
+
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
