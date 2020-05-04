45b9e7e542f74d82e12b3e0fd0cc3036f21ecefa
Mamadou Billo Diallo <billodiallonet@gmail.com>
2020-05-04 21:20:46 +0300

    config SQLALCHEMY

--- config.py
+++ config.py
@@ -2,10 +2,6 @@ import os
 
 class Config:
 
-    SECRET_KEY=os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://billo:123456@localhost/pitch'
-
-
     UPLOADED_PHOTOS_DEST ='app/static/photos'
 
     #  email configurations
